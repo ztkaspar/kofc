8887ee6ead523e3257020bcf519b901b6886a746
Zachary T. Kaspar <ztkaspar@outlook.com>
2014-03-12 13:42:34 -0500

    Finish layout and routes; minus sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Kofc::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/contact"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
