c7e2ee36962be9f58d5d8d5c921f33f445cb62ac
Zachary T. Kaspar <ztkaspar@outlook.com>
2014-03-12 09:03:36 -0500

    Initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kofc::Application.config.secret_key_base = 'dc967c738f5bd7ae40cbaa05e9101117ad2dea97e955af1faf2be3ff8351ab7c768bdc956f8e54eadac888ac04b7e3162fd94ca57e2213d57abbc403b168609e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kofc::Application.config.session_store :cookie_store, key: '_kofc_session'
